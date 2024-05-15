cafaf8886eb5625c4e44b22a5ba19bcafd6f2382
Gabriel Berges <gbergescoder@gmail.com>
2024-05-15 12:09:59 -0300

    Update database.js

--- DESAFIO-REFACTOR-LOGIN/src/database.js
+++ DESAFIO-REFACTOR-LOGIN/src/database.js
@@ -1,9 +1,5 @@
 const mongoose = require("mongoose");
 
-// mongoose.connect("mongodb+srv://gabrielberges97:URxbi4yQzEsPf1Xs@cluster0.q72hnhr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-//     .then(() => console.log("Conexión exitosa!"))
-//     .catch((error) => console.log("Hubo un error en la conexión", error)) //BD del jefecito
-
 mongoose.connect("mongodb+srv://slandini:coderback@cluster0.dcr0wh1.mongodb.net/E-commerce?retryWrites=true&w=majority&appName=Cluster0")
     .then(() => console.log("Conexión exitosa!"))
     .catch((error) => console.log("Hubo un error en la conexión", error))
